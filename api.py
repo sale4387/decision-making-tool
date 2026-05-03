394ccb46fd5c4137137578be25838be4beea1624
Sale <aleksandar.m.markovic@gmail.com>
2026-05-03 22:39:28 +0200

    added limit of request per ip

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from dotenv import load_dotenv
 load_dotenv()
@@ -7,7 +7,7 @@ from config import PRIMARY_MODEL_PROVIDER, SECONDARY_MODEL_PROVIDER
 from validation import validate_response_default
 import time
 
-class Request(BaseModel):
+class DecisionRequest(BaseModel):
     user_input: str
 
 app = FastAPI()
@@ -17,7 +17,7 @@ def root():
     return {"message": "API is running"}
 
 @app.post("/decision")
-def decision(request:Request):
+def decision(request:Request, body:DecisionRequest):
     ip = request.client.host
 
     if is_rate_limited(ip):
@@ -32,8 +32,8 @@ def decision(request:Request):
     provider=PRIMARY_MODEL_PROVIDER
 
     test_results,session,primary_client, secondary_client, input_based_on_mode=init_test_case("default")
-    prompt=prepare_test_case(request.user_input, input_based_on_mode,"default")
-    test_status, parsed_data, duration, error_type, number_of_tries, validation_errors, model_calls,test_score = run_test_case(primary_client, prompt, validate_response_default, request.user_input)
+    prompt=prepare_test_case(body.user_input, input_based_on_mode,"default")
+    test_status, parsed_data, duration, error_type, number_of_tries, validation_errors, model_calls,test_score = run_test_case(primary_client, prompt, validate_response_default, body.user_input)
 
 
 
@@ -46,12 +46,12 @@ def decision(request:Request):
             provider=SECONDARY_MODEL_PROVIDER
             isFallback="yes"
 
-            test_status, parsed_data, duration, error_type, number_of_tries, validation_errors, model_calls,test_score = run_test_case(secondary_client, prompt, validate_response_default, request.user_input)
+            test_status, parsed_data, duration, error_type, number_of_tries, validation_errors, model_calls,test_score = run_test_case(secondary_client, prompt, validate_response_default, body.user_input)
 
             if test_status=="failed":
                  parsed_data=None
 
-    process_test_results(test_status,test_results,session,"default", request.user_input, number_of_tries, error_type,validation_errors, duration,parsed_data,PRIMARY_MODEL_PROVIDER,model_calls,isFallback,test_score)
+    process_test_results(test_status,test_results,session,"default", body.user_input, number_of_tries, error_type,validation_errors, duration,parsed_data,PRIMARY_MODEL_PROVIDER,model_calls,isFallback,test_score)
     finalize_test_run("default",test_results,session)
     end_time = time.time()
     total_duration = f"{end_time - start_time:.2f}"
